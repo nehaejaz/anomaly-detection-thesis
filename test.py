e7377b4fce0965ec9429fba84f4c67af952c85c7
Neha Ejaz <nejaz@gpu03.science.uoit.ca>
2023-07-19 17:08:49 -0400

    added comments

--- test.py
+++ test.py
@@ -17,6 +17,10 @@ from sklearn.metrics import roc_auc_score
 from scipy.ndimage import gaussian_filter
 from collections import OrderedDict
 import warnings
+import csv
+import torchvision.transforms as transforms
+from PIL import Image
+
 warnings.filterwarnings("ignore")
 use_cuda = torch.cuda.is_available()
 device = torch.device('cuda' if use_cuda else 'cpu')
@@ -51,7 +55,7 @@ def main():
     PRED = Predictor().to(device)
 
     # load models
-    CKPT_name = f'./save_checkpoints/rotation_scale/{args.shot}/{args.obj}/{args.obj}_{args.shot}_rotation_scale_model.pt'
+    CKPT_name = f'./save_checkpoints/{args.shot}/{args.obj}/{args.obj}_{args.shot}_rotation_scale_model.pt'
     model_CKPT = torch.load(CKPT_name)
     STN.load_state_dict(model_CKPT['STN'])
     ENC.load_state_dict(model_CKPT['ENC'])
@@ -73,22 +77,29 @@ def main():
         print('Round {}:'.format(inference_round))
         scores_list, test_imgs, gt_list, gt_mask_list = test(args, models, inference_round, fixed_fewshot_list, test_loader, **kwargs)
         scores = np.asarray(scores_list)
+
         # Normalization
         max_anomaly_score = scores.max()
         min_anomaly_score = scores.min()
         scores = (scores - min_anomaly_score) / (max_anomaly_score - min_anomaly_score)
-
+       
         # calculate image-level ROC AUC score
         img_scores = scores.reshape(scores.shape[0], -1).max(axis=1)
         gt_list = np.asarray(gt_list)
         img_roc_auc = roc_auc_score(gt_list, img_scores)
         image_auc_list.append(img_roc_auc)
+        print("img_roc_auc", img_roc_auc)
+        print("image_auc_list",image_auc_list)
 
         # calculate per-pixel level ROCAUC
         gt_mask = np.asarray(gt_mask_list)
         gt_mask = (gt_mask > 0.5).astype(np.int_)
         per_pixel_rocauc = roc_auc_score(gt_mask.flatten(), scores.flatten())
         pixel_auc_list.append(per_pixel_rocauc)
+        print("per_pixel_rocauc", per_pixel_rocauc)
+        print("pixel_auc_list", pixel_auc_list)
+
+
 
     image_auc_list = np.array(image_auc_list)
     pixel_auc_list = np.array(pixel_auc_list)
@@ -110,6 +121,7 @@ def test(args, models, cur_epoch, fixed_fewshot_list, test_loader, **kwargs):
     test_outputs = OrderedDict([('layer1', []), ('layer2', []), ('layer3', [])])
 
     support_img = fixed_fewshot_list[cur_epoch]
+    
     augment_support_img = support_img
     # rotate img with small angle
     for angle in [-np.pi/4, -3 * np.pi/16, -np.pi/8, -np.pi/16, np.pi/16, np.pi/8, 3 * np.pi/16, np.pi/4]:
@@ -130,7 +142,6 @@ def test(args, models, cur_epoch, fixed_fewshot_list, test_loader, **kwargs):
         rotate90_img = rot90_img(support_img, angle)
         augment_support_img = torch.cat([augment_support_img, rotate90_img], dim=0)
     augment_support_img = augment_support_img[torch.randperm(augment_support_img.size(0))]
-
     # torch version
     with torch.no_grad():
         support_feat = STN(augment_support_img.to(device))
@@ -150,6 +161,8 @@ def test(args, models, cur_epoch, fixed_fewshot_list, test_loader, **kwargs):
     # calculate multivariate Gaussian distribution
     B, C, H, W = embedding_vectors.size()
     embedding_vectors = embedding_vectors.view(B, C, H * W)
+    support_set_embeddings=embedding_vectors
+
     mean = torch.mean(embedding_vectors, dim=0)
     cov = torch.zeros(C, C, H * W).to(device)
     I = torch.eye(C).to(device)
@@ -163,7 +176,7 @@ def test(args, models, cur_epoch, fixed_fewshot_list, test_loader, **kwargs):
     mask_list = []
     score_map_list = []
 
-    for (query_img, _, mask, y) in tqdm(test_loader):
+    for (query_img, support_img, mask, y) in tqdm(test_loader):
         query_imgs.extend(query_img.cpu().detach().numpy())
         gt_list.extend(y.cpu().detach().numpy())
         mask_list.extend(mask.cpu().detach().numpy())
@@ -195,23 +208,64 @@ def test(args, models, cur_epoch, fixed_fewshot_list, test_loader, **kwargs):
     B, C, H, W = embedding_vectors.size()
     embedding_vectors = embedding_vectors.view(B, C, H * W)
     dist_list = []
+    
+    print("support_set_embeddings", support_set_embeddings.shape)
+    print("support_set_embeddings", embedding_vectors.shape)
 
     for i in range(H * W):
         mean = train_outputs[0][:, i]
         conv_inv = torch.linalg.inv(train_outputs[1][:, :, i])
         dist = [mahalanobis_torch(sample[:, i], mean, conv_inv) for sample in embedding_vectors]
         dist_list.append(dist)
+        
+    # list_tensor = []
+    # for dis in dist_list:
+    #     # print(type(dis))
+    #     for d in dis:
+    #         d = d.cpu()
+    #         list_tensor.append(d.numpy())
 
+    # array_list = stacked_tensor.numpy()
+    # list_tensor = [array.reshape(1, -1).tolist() for array in list_tensor]
+
+    # # # Define the CSV file path
+    # csv_file = 'distances.csv'
+
+    # # # Write the data to the CSV file
+    # with open(csv_file, 'w', newline='') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerows(list_tensor)
+
+    # print("CSV file created successfully.")
+
+    """shape of dist_list is (83,56,56)"""
     dist_list = torch.tensor(dist_list).transpose(1, 0).reshape(B, H, W)
+    print("dist_list",dist_list.shape)
 
     # upsample
+    """shape of score_map is (83,224,224)"""
     score_map = F.interpolate(dist_list.unsqueeze(1), size=query_img.size(2), mode='bilinear',
                               align_corners=False).squeeze().numpy()
+  
+    print("score_map",score_map.shape)
 
     # apply gaussian smoothing on the score map
     for i in range(score_map.shape[0]):
         score_map[i] = gaussian_filter(score_map[i], sigma=4)
-
+        
+    """To Generate the Heat Maps. Basically the score_map
+    is the score of the patchesvwhere the anomalies are present."""    
+    # for i in range(score_map.shape[0]):
+    #     image = score_map[i, :, :]
+    #     print("image", image)
+    #     # Convert tensor to PIL Image
+    #     transform = transforms.ToPILImage()
+    #     image = transform(image)
+    #     # Convert image to grayscale
+    #     image_gray = image.convert('L')
+    #     # Save the image
+    #     image_gray.save("rounds/"+str(cur_epoch)+"/" + str(i)+".jpg")
+        
     return score_map, query_imgs, gt_list, mask_list
 
 if __name__ == '__main__':
