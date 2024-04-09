a780670136e2605adbacf739da8a6ddd47476bdc
nehaejaz <nehaejaz29@gmail.com>
2024-04-08 22:53:42 -0400

    removed local dir code

--- pages/View_Support_Sets.py
+++ pages/View_Support_Sets.py
@@ -16,31 +16,9 @@ credentials = ClientSecretCredential(
     tenant_id= tenant_id
 )
 
-def get_supp_set_list():
-    support_set_folder = "./support_sets"
-    return os.listdir(support_set_folder)
 
 def get_supp_set_list_from_blob(container_client):
     return container_client.list_blobs()   
-
-def list_supp_sets():
-    support_set_folder = "./support_sets"
-    count = 1
-    supp_set_list = get_supp_set_list()
-    st.header("Support Sets")
-    for index, supp_set_name in enumerate(supp_set_list):
-        col1, col2 = st.columns(2)
-        with col1:
-            st.write(f"{count}. {supp_set_name}")
-        with col2:
-            if st.button("Delete", type="primary", key=count):
-                #delete that set
-                file_path = os.path.join(support_set_folder, supp_set_name)
-                os.remove(file_path)
-                # Refresh the list
-                supp_set_list = get_supp_set_list()
-                st.experimental_rerun()  # Rerun the script to update the UI
-        count +=1
 
 def list_supp_sets_from_blob():
     container_name = 'support-sets'
